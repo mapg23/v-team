c7e293c8b3964cd17539e3315494233f7c579468
SIMON-5000 <sidr24@student.bth.com>
2025-11-14 22:19:57 +0100

    make volumes work for code mode removed '/usr' in node_modules path

--- bike/index.mjs
+++ bike/index.mjs
@@ -1,20 +1,18 @@
 "use strict";
-import express from 'express';
-import 'dotenv';
+import express from "express";
+import "dotenv";
 
 const app = express();
 
 const port = process.env.BIKE_PORT || 7071;
 
-
-
-app.get('/', (req, res) => {
-    res.send({
-        status: 200,
-        msg: "Bike"
-    })
+app.get("/", (req, res) => {
+  res.send({
+    status: 200,
+    msg: "Bike OK",
+  });
 });
 
 app.listen(port, function () {
-    console.log(`Listening on port: ${port}`);
+  console.log(`Listening on port: ${port}`);
 });
