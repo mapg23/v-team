066496b78f9b758ae56217d45ced2b12b2434526
mapg23 <mapg23@student.bth.se>
2025-11-18 15:33:13 +0100

    fixed port

--- admin-client/vite.config.js
+++ admin-client/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
+  server:{
+    host: '0.0.0.0',
+    port: process.env.ADMIN_CLIENT_PORT || 5173,
+    hmr: {
+      clientPort: process.env.ADMIN_CLIENT_PORT || 5173
+    }
+  }
 })
