f640e6cb5b48c841bde2572f5bfb9f554642a50b
mapg23 <mapg23@student.bth.se>
2025-11-19 10:25:05 +0000

    removed unusde file and added docummentation

--- bike/simulator.mjs
+++
@@ -1,8 +0,0 @@
-import { parentPort } from "worker_threads";
-
-console.log("WORKER STARTED!");
-
-parentPort.on("message", (msg) => {
-  console.log("Worker received:", msg);
-  parentPort.postMessage({ got: msg });
-});
