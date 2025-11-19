1b03dbd8b180ca6d8cd6c9e153f9626f14d4c8e8
mapg23 <mapg23@student.bth.se>
2025-11-19 17:07:59 +0000

    updated how handling of calls made to the thread worker and fixed bugs

--- bike/Simulator.mjs
+++ bike/Simulator.mjs
@@ -6,6 +6,10 @@ class Simulator {
     bikes = [];
     total_bikes = 100;
     start() {
+        if (this.bikes.length >= this.total_bikes) {
+            return { event: `Bikes already at max capacity: ${this.bikes.length}/${this.total_bikes}`};
+        }
+
         for(let i = 0; i < this.total_bikes; i++) {
             this.bikes.push(new Device(i, {x: 0, y:0}))
         }
@@ -13,48 +17,66 @@ class Simulator {
     }
 
     end() {
+        this.bikes = [];
         return { event: 'stopping worker'};
     }
     list() {
         return { event: 'Listing all bikes', data : this.bikes};
     }
 
-    move(payload) {
+    getBike(payload) {
+        let id = payload['data'].id;
+        return this.bikes[id];
+    }
+
+    /**
+     * Method that alters a specific bikes cordinates based on bike id.
+     * @param {Array} payload 
+     * @returns {Array} - Array of result.
+     */
+    move(bike) {
         if (this.bikes.length == 0) {
             this.start();
         }
 
-        console.log(payload)
-        return { event: `Payload: ${cords}`}
+        const returnMsg = { event: `Changed bike: ${bike.id} from pos: ${this.bikes[bike.id].cords} to: {x: ${bike.x}, y: ${bike.y}} `}
+        
+        this.bikes[bike.id].cords = {
+            x: Number(bike.x),
+            y: Number(bike.y)
+        };
 
-        // returnMsg = { event: `Altered Bike: ${id} from location: ${this.bikes[id].cords} to: ${cords} ` }
-        // this.bikes[id].cords = cords;
-        // return returnMsg
+        return returnMsg;
     }
 };
 
-
+// Instance of Simulator, this is active while the main thread is.
 const simm = new Simulator();
 
-parentPort?.on("message", (msg) => {
+/**
+ * Routing from the main application into the simulator class.
+ */
+parentPort?.on("message", async (msg) => {
     const { id, cmd, payload } = msg;
-    let res;
-    switch(msg.cmd) {
-        case 'start-job':
-            res = simm.start();
-            parentPort.postMessage({ id, ...res})
-        break;
-        case 'end-job':
-            res = simm.end()
-            parentPort.postMessage({ id, ...res})
-        break;
-        case 'list':
-            res = simm.list();
-            parentPort.postMessage({ id, ...res});
-        break;
-        case 'move':
-            res = simm.move(payload);
-            parentPort.postMessage({ id, ...res});
-        break;
+
+    const routers = {
+        'start-job': () => simm.start(),
+        'end-job': () => simm.end(),
+        'list': () => simm.list(),
+        'move': () => simm.move(payload),
+        'get-bike': () => simm.getBike(payload),
+    };
+
+    const callFunction = routers[cmd];
+
+    if (!callFunction) {
+        return parentPort.postMessage({id, error: `Unknown call ${cmd}`});
+    }
+
+    try {
+        const res = await callFunction();
+        parentPort.postMessage({ id, ...res});
+    } catch (error) {
+        parentPort.postMessage({id, error: error.message});
     }
 });
