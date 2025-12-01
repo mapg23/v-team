adfe1a4ea01f159b5a59e1a1a685bcc64d8f412d
Kristoffer <kristoffer.ohlund@icloud.com>
2025-12-01 09:58:07 +0100

    Add LoginView

---
+++ admin-client/src/views/auth/LoginView.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import LoginForm from "../../components/forms/LoginForm"
+import authService from "../../services/auth"
+
+export default function LoginView() {
+  const navigate = useNavigate(); // redirect på React vis
+
+  async function handleLogin({email, password}) {
+    console.log("haneLogin in loginView")
+    const success = await authService.login({email, password});
+
+    if (success) {
+      navigate("/home"); // Redirect!
+    }
+  }
+
+  return <LoginForm onFormSubmit={handleLogin} />;
+}
