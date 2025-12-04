399c0aa7868b9e3d07d44c7e32e11ebaa33b29c4
Kristoffer <kristoffer.ohlund@icloud.com>
2025-12-04 11:55:45 +0100

    Add nominatim

---
+++ admin-client/src/services/nominatim.jsx
@@ -0,0 +1,14 @@
+/**
+ * Service to return map objects
+ * @param {string} address as string
+ * @returns json
+ */
+
+export default async function getCoordinates(address) {
+    const urlEncodedAddress = encodeURIComponent(address);
+    const url = "https://nominatim.openstreetmap.org/search.php?format=jsonv2&q=";
+    const response = await fetch(`${url}${urlEncodedAddress}`);
+    const result = await response.json();
+
+    return result;
+}
