10ac2f36a0c2f9eea0d3069068b26eef150401bb
Kristoffer <kristoffer.ohlund@icloud.com>
2025-12-05 15:24:07 +0100

    Update CityTable

--- admin-client/src/components/table/CityTable.jsx
+++ admin-client/src/components/table/CityTable.jsx
@@ -1,24 +1,28 @@
+/**
+ * Create a Table based on a array of objects
+ * @param {Array} cityDetails array of objects
+ * @returns 
+ */
 export default function CityTable({ cityDetails }) {
+  if (!Array.isArray(cityDetails) || cityDetails.length === 0) return null;
 
-  // Låt cityDetails alltid vara en array av objekt
-  const data = Array.isArray(cityDetails) ? cityDetails : [cityDetails];
-
-  if (data.length === 0) return null;
+  const headers = Object.keys(cityDetails[0]);
 
   return (
     <table>
       <thead>
         <tr>
-          {Object.keys(data[0]).map((key) => (
-            <th>{key.toUpperCase()}</th>
+          {headers.map((key) => (
+            <th key={key}>{key.toUpperCase()}</th>
           ))}
         </tr>
       </thead>
+
       <tbody>
-        {data.map((city, index) => (
-          <tr key={index}>
-            {Object.values(city).map((value, i) => (
-              <td key={i}>{value}</td>
+        {cityDetails.map((city) => (
+          <tr key={city.id}>
+            {headers.map((key) => (
+              <td key={city.id + "-" + key}>{city[key]}</td>
             ))}
           </tr>
         ))}
