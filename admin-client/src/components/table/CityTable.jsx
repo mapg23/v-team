c27930153a75f3c3a01b48a8f42c716313affeeb
Kristoffer <kristoffer.ohlund@icloud.com>
2025-12-05 10:08:49 +0100

    Make all cityObjects arrays
    To enable the table to dynamically render one or multiple cityDetails objects,
    we turn them into an array. If array is empty we return null

--- admin-client/src/components/table/CityTable.jsx
+++ admin-client/src/components/table/CityTable.jsx
@@ -1,16 +1,28 @@
-export default function CityTable({cityDetails}) {
-    return (
-      <table>
+export default function CityTable({ cityDetails }) {
+
+  // Låt cityDetails alltid vara en array av objekt
+  const data = Array.isArray(cityDetails) ? cityDetails : [cityDetails];
+
+  if (data.length === 0) return null;
+
+  return (
+    <table>
+      <thead>
         <tr>
-          {Object.keys(cityDetails).map((key) => (
+          {Object.keys(data[0]).map((key) => (
             <th>{key.toUpperCase()}</th>
           ))}
         </tr>
-        <tr>
-          {Object.values(cityDetails).map((value) => (
-            <td>{value}</td>
-          ))}
-        </tr>
-      </table>
-    );
+      </thead>
+      <tbody>
+        {data.map((city, index) => (
+          <tr key={index}>
+            {Object.values(city).map((value, i) => (
+              <td key={i}>{value}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
