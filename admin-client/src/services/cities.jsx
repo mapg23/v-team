f71c3b72c8ccdafa63e40c31a106fbe70a2051d0
Kristoffer <kristoffer.ohlund@icloud.com>
2025-12-05 10:19:58 +0100

    Update cityService API

--- admin-client/src/services/cities.jsx
+++ admin-client/src/services/cities.jsx
@@ -39,6 +39,33 @@ const cityService = {
   },
 
   /**
+   * Add new city in database
+   * @returns {Object} new city
+   */
+  addNewCity: async function addNewCity() {
+    const newCity = {};
+    try {
+      const response = await fetch(`${API}/cities`, {
+        body: JSON.stringify(newCity),
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error on addNewCity! Status: ${response.status}`);
+      }
+
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  },
+
+  /**
    * Get City details
    * @param {int} id for the city
    * @return {Object} { "id": 1, "name": "Stockholm", "stations": 5, "bikes": 240 }
@@ -71,13 +98,12 @@ const cityService = {
   /**
    * Get all bikes in city
    * @param {int} id city id
-   * @return {Object}
-   */
-  getBikesInCity: async function getBikesInCity(id) {
-    return [
+   * @return {Array} [
       { id: 1, cityId: 1, status: 100, battery: 85, lat: 59.33, lng: 18.07 },
       { id: 2, cityId: 1, status: 400, battery: 60, lat: 59.35, lng: 18.08 },
     ];
+   */
+  getAllBikesInCity: async function getAllBikesInCity(id) {
     try {
       const response = await fetch(`${API}/cities/${id}/bikes`, {
         method: "GET",
@@ -100,7 +126,36 @@ const cityService = {
     }
   },
 
+  /**
+   * Get all bikes in city
+   * @param {int} id city id
+   * @return {Array} [
+      { id: 1, cityId: 1, status: 100, battery: 85, lat: 59.33, lng: 18.07 },
+      { id: 2, cityId: 1, status: 400, battery: 60, lat: 59.35, lng: 18.08 },
+    ];
+   */
+  getSingleBikeInCity: async function getSingleBikeInCity(cityId, bikeId) {
+    try {
+      const response = await fetch(`${API}/cities/${cityId}/bikes/${bikeId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
+      if (!response.ok) {
+        throw new Error(
+          `HTTP error on getAllCities! Status: ${response.status}`
+        );
+      }
+
+      const responseData = await response.json();
+      return responseData;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  },
 };
 
 
