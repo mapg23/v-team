4e21c32d4eef3fc8a595f597437455afed9530f0
Kristoffer <kristoffer.ohlund@icloud.com>
2025-12-06 12:36:43 +0100

    Add LoginWithGithub functionality

--- admin-client/src/App.jsx
+++ admin-client/src/App.jsx
@@ -4,14 +4,32 @@ import LoginView from "./views/auth/LoginView";
 import HomeView from "./views/home/HomeView";
 import Navbar from "./components/nav/Nav";
 import { useState } from "react";
+import GithubCallback from "./components/auth/GithubCallback";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  return (
+    <div className="app-layout">
+      <Navbar />
+      <div className="app-content">
+        <Router>
+          <Routes>
+            <Route path="/" element={<LoginView />}></Route>
+            <Route path="/login/github/callback" element={<GithubCallback />} />
+            <Route path="/welcome" element={<HomeView />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    </div>
+  );
+
   if (!loggedIn) {
     return (
       <Router>
         <Routes>
           <Route path="/" element={<LoginView />}></Route>
+          <Route path="/login/github/callback" element={<GithubCallback />} />
+          <Route path="/welcome" element={<HomeView />}></Route>
         </Routes>
       </Router>
     );
@@ -22,7 +40,7 @@ function App() {
       <div className="app-content">
         <Router>
           <Routes>
-            <Route path="/" element={<HomeView />}></Route>
+            <Route path="/welcome" element={<HomeView />}></Route>
           </Routes>
         </Router>
       </div>
