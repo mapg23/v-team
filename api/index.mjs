bf111d03960c9b1995b905e4a2de9e2255c55dbe
Kristoffer <kristoffer.ohlund@icloud.com>
2025-12-11 10:11:01 +0100

    Add response to /telemetry route

--- api/index.mjs
+++ api/index.mjs
@@ -6,14 +6,14 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 
 import authRoutes from "./src/routes/authRoutes.mjs";
-import createUserRouter from './src/routes/userRoutes.mjs';
-import createCityRouter from './src/routes/cityRoutes.mjs';
-import createBikeRouter from './src/routes/bikeRoutes.mjs';
-import startSimulator from './src/startSimulator.mjs';
+import createUserRouter from "./src/routes/userRoutes.mjs";
+import createCityRouter from "./src/routes/cityRoutes.mjs";
+import createBikeRouter from "./src/routes/bikeRoutes.mjs";
+import startSimulator from "./src/startSimulator.mjs";
 
 const app = express();
 const port = process.env.API_PORT || 9091;
-const version = process.env.API_VERSION || 'v1';
+const version = process.env.API_VERSION || "v1";
 const jwtSecret = process.env.JWT_SECRET;
 
 // Middleware
@@ -21,14 +21,13 @@ app.use(cors({ origin: "*" }));
 app.set("json spaces", 2);
 app.use(express.json());
 // Döljer Express-version
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // ----------- Routes
 // app.use(`/api/${version}`, authRoutes);
 app.use(`/api/${version}`, createUserRouter());
 app.use(`/api/v1/auth`, authRoutes);
 app.use(`/api/${version}`, createCityRouter());
-
 
 // -------- Socket.io
 const server = createServer(app);
@@ -41,31 +40,32 @@ const io = new Server(server, {
 
 // Om godkänd klient
 io.on("connection", (socket) => {
-  console.log("connected!", socket.id)
+  console.log("connected!", socket.id);
 });
 
-
-app.get('/', (req, res) => {
-    res.redirect(`/api/${version}/cities`);
+app.get("/", (req, res) => {
+  res.redirect(`/api/${version}/cities`);
 });
 
 app.use(`/api/${version}`, createBikeRouter(io));
 
 // Telemetry route (WebSocket)
-app.post('/telemetry', (req, res) => {
-    const bikes = req.body.bikes || null;
+app.post("/telemetry", (req, res) => {
+  const bikes = req.body.bikes || null;
 
-    if (!bikes) {
-        return res.status(400).json({ error: 'No bikes' });
-    }
+  if (!bikes) {
+    return res.status(400).json({ error: "No bikes" });
+  }
 
-    io.emit('bikes', bikes);
+  io.emit("bikes", bikes);
+  // Skicka något svar så klienten inte hänger
+  res.status(200).json({ ok: true });
 });
 
 // Startar server med Socket.IO
 server.listen(port, async () => {
-    console.log(`Server is listening on port: ${port}`);
+  console.log(`Server is listening on port: ${port}`);
 
-    // Här startar vi simulatorn direkt när servern är uppe
-    await startSimulator();
+  // Här startar vi simulatorn direkt när servern är uppe
+  await startSimulator();
 });
