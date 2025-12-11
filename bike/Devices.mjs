39556792445d48174c7bd6576722430072d0b3fb
Kristoffer <kristoffer.ohlund@icloud.com>
2025-12-11 13:44:48 +0100

    Add city_id to constructor

--- bike/Devices.mjs
+++ bike/Devices.mjs
@@ -4,33 +4,41 @@
  * Device Class, used to simulate a bike.
  */
 class Device {
-    constructor(id, cords, battery = 100, status = 10, occupied = false, speed = 0) {
-        this.id = id;
-        this.cords = cords;
-        this.battery = battery;
-        this.status = status;
-        this.occupied = occupied;
-        this.speed = speed;
+  constructor(
+    id,
+    cords,
+    city_id, 
+    battery = 100,
+    status = 10,
+    occupied = false,
+    speed = 0
+  ) {
+    this.id = id;
+    this.cords = cords;
+    this.city_id = city_id;
+    this.battery = battery;
+    this.status = status;
+    this.occupied = occupied;
+    this.speed = speed;
+  }
+
+  move(cords) {
+    if (this.status > 50) {
+      return { event: "bike is unable to move, reason:", data: this.status };
     }
 
-    move(cords) {
-        if (this.status > 50) {
-            return { event: 'bike is unable to move, reason:', data: this.status};
-        }
+    this.cords = cords;
+  }
 
-        this.cords = cords;
+  setStatus(status) {
+    this.status = status;
+  }
+
+  selfDiagnose() {
+    if (this.battery <= 20) {
+      this.setStatus(50); // low battery
     }
-
-    setStatus(status) {
-        this.status = status;
-    }
-
-    selfDiagnose() {
-        if (this.battery <= 20) {
-            this.setStatus(50) // low battery
-        }
-    }
-
+  }
 }
 
 export default Device;
