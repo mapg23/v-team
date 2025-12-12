774805513d73d0ecec9e5b3a8d49a4704e0c1243
mapg23 <mapg23@student.bth.se>
2025-12-12 16:32:57 +0100

    Update

--- bike/src/Devices.mjs
+++ bike/src/Devices.mjs
@@ -9,54 +9,62 @@
  * =====================================================================
  */
 class Device {
-    constructor(
-        id,
-        cords,
-        battery = 100,
-        status = 10,
-        occupied = false,
-        speed = 0,
-        city_id = null,
-    ) {
-        this.id = id;
-        this.cords = cords;
-        this.battery = battery;
-        this.status = status;
-        this.occupied = occupied;
-        this.speed = speed; // unused for now
-        this.city_id = city_id;
-    }
+  constructor(
+    id,
+    cords,
+    battery = 100,
+    status = 10,
+    occupied = false,
+    speed = 0,
+    city_id = null,
+  ) {
+    this.id = id;
+    this.cords = cords;
+    this.battery = battery;
+    this.status = status;
+    this.occupied = occupied;
+    this.speed = speed; // unused for now
+    this.city_id = city_id;
+  }
 
-    /**
-   * Method that changes bike position
-   * @param {Array} cords - X and Y cord to change to
-   * @returns Void
+  /**
+   * Getter for id
+   * @returns {Number} - id
    */
-    move(cords) {
-        if (this.status > 50) {
-            return { event: "bike is unable to move, reason:", data: this.status };
-        }
+  getId() {
+    return this.id;
+  }
 
-        this.cords = cords;
+  /**
+ * Method that changes bike position
+ * @param {Array} cords - X and Y cord to change to
+ * @returns Void
+ */
+  move(cords) {
+    if (this.status > 50) {
+      return { event: "bike is unable to move, reason:", data: this.status };
     }
 
-    /**
-   * Method to change status.
-   * @param {Number} status - Number that represents a state
-   */
-    setStatus(status) {
-        this.status = status;
-    }
+    this.cords = cords;
+  }
 
-    /**
-   * Method that will change status depending on health of bike
-   * TODO: ADD MORE CONSTRAINS TO STATUS
-   */
-    selfDiagnose() {
-        if (this.battery <= 20) {
-            this.setStatus(50); // low battery
-        }
+  /**
+ * Method to change status.
+ * @param {Number} status - Number that represents a state
+ */
+  setStatus(status) {
+    this.status = status;
+  }
+
+  /**
+ * Method that will change status depending on health of bike
+ * TODO: ADD MORE CONSTRAINS TO STATUS
+ */
+  selfDiagnose() {
+    if (this.battery <= 20) {
+      this.setStatus(50); // low battery
     }
+  }
 }
 
 export default Device;
