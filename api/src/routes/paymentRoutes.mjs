9a0441374a4d7a54ed14af70443435091fd904ce
SIMON-5000 <sidr24@student.bth.com>
2025-12-15 15:37:43 +0100

    correct spelling  in route

--- api/src/routes/paymentRoutes.mjs
+++ api/src/routes/paymentRoutes.mjs
@@ -7,30 +7,42 @@ const stripe = new Stripe(`${process.env.STRIPE_SECRET}`);
 const router = express.Router();
 
 /**
- * Create a Stripe checkout session
+ * Create a Stripe payment intent
  * Request body needs:
  * ID for the user
  * ID for the bike
  * @returns {Array} an array with the trip object.
  */
-router.post(`/pay-with-stripe`,
+router.post(`/create-intent`,
     // validation.idBody,
     // validation.checkValidationResult,
     async (req, res) => {
-        const session = await stripe.checkout.sessions.create({
-            line_items: [
-                {
-                    // Provide the exact Price ID (for example,
-                    // price_1234) of the product you want to sell
-                    price: '{{price_1SeWhVEthuRq4UFn2vzOqCiF}}',
-                    quantity: 1,
-                },
-            ],
-            mode: 'payment',
-            success_url: `localhost:5173/payment?success=true`,
-        });
+        const { amount } = req.body;
 
-        res.redirect(303, session.url);
+        console.log("initializing create-intent for: ", amount);
+        try {
+        // Create a PaymentIntent with the order amount and currency
+            const paymentIntent = await stripe.paymentIntents.create({
+                // amount: calculateOrderAmount(items),
+                amount: amount,
+                currency: "sek",
+                // In the latest version of the API, specifying the
+                // `automatic_payment_methods` parameter is optional
+                // because Stripe enables its functionality by default.
+                automatic_payment_methods: {
+                    enabled: true,
+                },
+            });
+
+            console.log(paymentIntent);
+
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        } catch (err) {
+            console.error('Error creating Stripe payment intent: ', err);
+            res.status(500).json({ error: err.message });
+        }
     });
 
 export default router;
