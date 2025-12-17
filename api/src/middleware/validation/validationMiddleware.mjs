baf5940a2b8ace88e15f4ac96d73417d831986f5
SIMON-5000 <sidr24@student.bth.com>
2025-12-17 16:58:36 +0100

    resolving rebase

--- api/src/middleware/validation/validationMiddleware.mjs
+++ api/src/middleware/validation/validationMiddleware.mjs
@@ -15,13 +15,7 @@ import { validationResult, body, param } from "express-validator";
  * @returns {object|function} response | newxt()
  */
 export const checkValidationResult = (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-    const errors = validationResult(req);
+    let errors = validationResult(req);
 
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
