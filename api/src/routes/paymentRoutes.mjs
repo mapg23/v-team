8f73a18bdf7b08ddc129102db7eca9fff379a83d
SIMON-5000 <sidr24@student.bth.com>
2025-12-19 11:29:03 +0100

    removed logs, and magic number

--- api/src/routes/paymentRoutes.mjs
+++ api/src/routes/paymentRoutes.mjs
@@ -8,28 +8,9 @@ const stripe = new Stripe(`${process.env.STRIPE_SECRET}`);
 const router = express.Router();
 
 /**
- * Get by user id
- */
-router.get('/user/:id', async (req, res) => {
-    const userId = req.params.id;
-
-    const wallet = await wallets.getWalletByUserId(userId);
-
-    return res.json(wallet);
-});
-
-router.get('/user/:id', async (req, res) => {
-    const userId = req.params.id;
-
-    const wallet = await wallets.getWalletByUserId(userId);
-
-    return res.json(wallet);
-});
-
-/**
  * Create a Stripe payment intent
- * Request body needs:
- * @returns {Array} an array with the trip object.
+ * Request body needs: amount in öre/cent/pence
+ * @returns the client secret
  */
 router.post(`/create-intent`,
     // validation.idBody,
@@ -62,7 +43,10 @@ router.post(`/create-intent`,
             res.status(500).json({ error: err.message });
         }
     });
-
+/**
+ * If payment is successfull, this route will add the sum to logged in users balance
+ * Returns the amount in swedish krona and the updated balance.
+ */
 router.post(`/payment-success`,
     // validation.payment,
     // validation.checkValidationResult,
@@ -75,8 +59,8 @@ router.post(`/payment-success`,
         try {
             const paymentIntent = await stripe.paymentIntents.retrieve(intentId);
             const {amount, status} = paymentIntent;
-            // Check currency?
-            // const expectedAmounts = [10000, 25000, 50000];
+            const amountInKrona = amount / 100;
+
 
             // console.log(paymentIntent, "*** status, amount: ", status, amount);
 
@@ -86,14 +70,13 @@ router.post(`/payment-success`,
 
             let walletRes = await wallets.getWalletByUserId(userId);
 
-            console.log("WalletRes: ", walletRes);
             if (!walletRes[0]) {
                 walletRes = await wallets.createWallet(userId);
             }
             // console.log("WalletRes: ", walletRes);
             const userWallet = walletRes[0];
 
-            const newBalance = userWallet.balance += (amount / 100);
+            const newBalance = userWallet.balance += (amountInKrona);
             const updateData = {
                 balance: newBalance
             };
@@ -106,70 +89,11 @@ router.post(`/payment-success`,
                 throw new Error("balance was not updated");
             }
 
-            return res.json({ balance: newBalance });
+            return res.json({ added: amountInKrona, balance: newBalance });
         } catch (err) {
             console.error('Error creating Stripe payment intent: ', err);
             res.status(500).json({ error: err.message });
         }
     });
-
-
-// USING STRIPES CLI IT WORKS ON LOCALHOST:
-// stripe listen --forward-to localhost:9091/api/v1/payments/webhook
-// Opens a secure tunnel for stripe.
-
-// // Replace this endpoint secret with your unique endpoint secret key
-// // If you're testing with the CLI, run 'stripe listen' to find the secret key
-// // If you defined your endpoint using the API or the Dashboard,
-// // check your webhook settings for your endpoint secret: https://dashboard.stripe.com/webhooks
-// const endpointSecret = process.env.STRIPE_WEBHOOK;
-
-// // The express.raw middleware keeps the request body unparsed;
-// // this is necessary for the signature verification process
-// router.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-//     console.log("webhook hooked up");
-//     let event;
-//     let paymentIntent;
-//     let paymentMethod;
-
-//     if (endpointSecret) {
-//     // Get the signature sent by Stripe
-//         const signature = request.headers['stripe-signature'];
-
-//         try {
-//             event = stripe.webhooks.constructEvent(
-//                 request.body,
-//                 signature,
-//                 endpointSecret
-//             );
-//         } catch (err) {
-//             console.log(`⚠️ Webhook signature verification failed.`, err.message);
-//             return response.sendStatus(400);
-//         }
-//         console.log(event);
-//         // Handle the event
-//         switch (event.type) {
-//             case 'payment_intent.succeeded':
-//                 paymentIntent = event.data.object;
-//                 // Then define and call a method to handle the successful payment intent.
-//                 // handlePaymentIntentSucceeded(paymentIntent);
-//                 console.log(paymentIntent);
-//                 break;
-//             case 'payment_method.attached':
-//                 paymentMethod = event.data.object;
-//                 // Then define and call a method to handle the
-//                 // successful attachment of a PaymentMethod.
-//                 // handlePaymentMethodAttached(paymentMethod);
-//                 console.log(paymentMethod);
-//                 break;
-//                 // ... handle other event types
-//             default:
-//                 console.log(`Unhandled event type ${event.type}`);
-//         }
-
-//         // Return a response to acknowledge receipt of the event
-//         response.json({received: true});
-//     }
-// });
 
 export default router;
