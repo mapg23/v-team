c8adf1190fd8c699ea8ab4c43cfb7fd167950f15
Kristoffer <kristoffer.ohlund@icloud.com>
2026-01-02 17:50:31 +0100

    Add getAllTrips api call

--- admin-client/src/services/trips.jsx
+++ admin-client/src/services/trips.jsx
@@ -5,6 +5,11 @@ import API from "../config/api.js";
  * Based on endpoints: https://docs.google.com/spreadsheets/d/1Tza3ZSUOJJRQJeSquKQbE6fRy4d3zNGafAVQxUVNg9M/edit?gid=0#gid=0
  */
 const TripService = {
+  /**
+   * Get trips by user
+   * @param {number} userId
+   * @returns
+   */
   getTripsByUserId: async function getTripsByUserId(userId) {
     try {
       const response = await fetch(`${API}/trips/user/${userId}`, {
@@ -17,6 +22,32 @@ const TripService = {
       if (!response.ok) {
         throw new Error(
           `HTTP error on getTripsByuserId! Status: ${response.status}`
+        );
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  },
+
+  /**
+   * Get all trips
+   * @returns json
+   */
+  getAllTrips: async function getAllTrips() {
+    try {
+      const response = await fetch(`${API}/trips`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `HTTP error on getAllTrips! Status: ${response.status}`
         );
       }
 
