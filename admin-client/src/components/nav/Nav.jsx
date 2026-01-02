d9d6f738795b6cd3a9d668f9405ca95a9ea18a97
Kristoffer <kristoffer.ohlund@icloud.com>
2026-01-02 10:00:16 +0100

    Add cardWrapper and card css classes

--- admin-client/src/components/nav/Nav.jsx
+++ admin-client/src/components/nav/Nav.jsx
@@ -12,34 +12,34 @@ function Navigation({ logout }) {
   }
   return (
     <>
-      <ul className={navStyles.navList}>
-        <li>
-          <a href="/welcome">Översikt</a>
-        </li>
-        <li>
-          <a href="/cities">Städer</a>
-        </li>
-        <li>
-          <a href="/cost">Prissättning</a>
-        </li>
-        <li>
-          <a href="/stations">Laddstationer</a>
-        </li>
-        <li>
-          <a href="/parkings">Parkeringar</a>
-        </li>
-        <li>
-          <a href="/users">Användare</a>
-        </li>
-        <li>
-          <a href="/bikes">Elsparkcyklar</a>
-        </li>
-        <li>
-          <button className={`${btnStyles.delete}`} onClick={handleLogout}>
-            Logout
-          </button>
-        </li>
-      </ul>
+        <ul className={navStyles.navList}>
+          <li>
+            <a href="/welcome">Översikt</a>
+          </li>
+          <li>
+            <a href="/cities">Städer</a>
+          </li>
+          <li>
+            <a href="/cost">Prissättning</a>
+          </li>
+          <li>
+            <a href="/stations">Laddstationer</a>
+          </li>
+          <li>
+            <a href="/parkings">Parkeringar</a>
+          </li>
+          <li>
+            <a href="/users">Användare</a>
+          </li>
+          <li>
+            <a href="/bikes">Elsparkcyklar</a>
+          </li>
+          <li>
+            <button className={`${btnStyles.delete}`} onClick={handleLogout}>
+              Logout
+            </button>
+          </li>
+        </ul>
     </>
   );
 }
