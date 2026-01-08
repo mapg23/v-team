bd6fd0d2a9a89b16a69023bc2100a0097ee40834
Kristoffer <kristoffer.ohlund@icloud.com>
2026-01-08 15:16:59 +0100

    Add zone data to popup

--- admin-client/src/components/map/react-draw/ChargingZoneMarkers.jsx
+++ admin-client/src/components/map/react-draw/ChargingZoneMarkers.jsx
@@ -9,8 +9,37 @@ export default function ChargingZoneMarkers({ zones }) {
   return (
     <>
       {zones.map((zone) => (
-        <Marker icon={chargingIcon} position={[zone.latitude, zone.longitude]}>
-          <Popup>Laddstation: {zone.name}</Popup>
+        <Marker key={zone.id} icon={chargingIcon} position={[zone.latitude, zone.longitude]}>
+          <Popup>
+            <table>
+              <tbody>
+                <tr key={zone.id}>
+                  <th>Stations Id:</th>
+                  <td>{zone.id}</td>
+                </tr>
+                <tr key="cityid">
+                  <th>Stads id:</th>
+                  <td>{zone.city_id}</td>
+                </tr>
+                <tr key="station">
+                  <th>Stations namn:</th>
+                  <td>{zone.name}</td>
+                </tr>
+                <tr key="lat">
+                  <th>Latitude:</th>
+                  <td>{zone.latitude}</td>
+                </tr>
+                <tr key="long">
+                  <th>Longitude</th>
+                  <td>{zone.longitude}</td>
+                </tr>
+                <tr key="cap">
+                  <th>Kapacitet</th>
+                  <td>{zone.capacity}</td>
+                </tr>
+              </tbody>
+            </table>
+          </Popup>
         </Marker>
       ))}
     </>
