6786662b3e2e3b75320956bc455dc70c2ab9c182
Kristoffer <kristoffer.ohlund@icloud.com>
2026-01-13 15:08:42 +0100

    Update css redirect class

--- admin-client/src/components/table/CitiesTable.jsx
+++ admin-client/src/components/table/CitiesTable.jsx
@@ -9,55 +9,66 @@ export default function CitiesTable({ data, action, inspect }) {
   const _data = Array.isArray(data) ? data : [data];
   if (_data.length === 0) return <p>Finns inga städer..</p>;
 
-    const headers = [
-      "City id",
-      "City name",
-      "Bikes in city",
-      "Parking zones",
-      "Charging zones",
-    ];
-  
-    /**
-     * Call action on object with id <id>
-     * @param {Int} objectId
-     */
-    function handleAction(objectId) {
-      action(objectId);
-    }
-  
-    /**
-     * Call action on object with id <id>
-     * @param {Int} objectId
-     */
-    function handleInspect(objectId) {
-      inspect(objectId);
-    }
-  
-    return (
-      <table className={styles.bikeTable}>
-        <thead>
-          <tr>
-            {headers.map((key) => (
-              <th key={key}>{key}</th>
-            ))}
-          </tr>
-        </thead>
-  
-        <tbody>
-          {_data.map((obj) => (
-            <tr onClick={() => handleInspect(obj.id)} key={obj.id}>
-              <td>{obj.id}</td>
-              <td>{obj.name}</td>
-              <td>{obj.bikeCount}</td>
-              <td>{obj.parkingCount}</td>
-              <td>{obj.stationCount}</td>
-              {action ?  <td key={obj.id}>
-                <button onClick={() => handleAction(obj.id)}>Delete Bike</button>
-              </td> : ""}
-             
-            </tr>
+  const headers = [
+    "City id",
+    "City name",
+    "Bikes in city",
+    "Parking zones",
+    "Charging zones",
+  ];
+
+  /**
+   * Call action on object with id <id>
+   * @param {Int} objectId
+   */
+  function handleAction(objectId) {
+    action(objectId);
+  }
+
+  /**
+   * Call action on object with id <id>
+   * @param {Int} objectId
+   */
+  function handleInspect(objectId) {
+    inspect(objectId);
+  }
+
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          {headers.map((key) => (
+            <th key={key}>{key}</th>
           ))}
-        </tbody>
-      </table>
-    );
+          {action ? <th>Action</th> : ""}
+        </tr>
+      </thead>
+
+      <tbody>
+        {_data.map((obj) => (
+          <tr key={obj.id}>
+            <td>{obj.id}</td>
+            <td
+              className={styles.redirect}
+              onClick={() => handleInspect(obj.id)}
+            >
+              {obj.name}
+            </td>
+            <td>{obj.bikeCount}</td>
+            <td>{obj.parkingCount}</td>
+            <td>{obj.stationCount}</td>
+            {action ? (
+              <td>
+                <button onClick={() => handleAction(obj.id)}>
+                  Delete City
+                </button>
+              </td>
+            ) : (
+              ""
+            )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
